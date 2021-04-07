7875099285904cbc6af7b92dfbd21cecd52ee6f1
karlsen <karlsenab7@gmail.com>
2021-04-07 12:41:32 +0700

    map alunalun

--- src/map.py
+++ src/map.py
@@ -15,6 +15,7 @@ def inputRouteAstar(path, route_Astar):
         node = g1.nodes[idxNode]
         route_Astar.append([node.x, node.y])
 
+
 def createMarkers(graph):
     # Create Markers
     i = 1
@@ -23,9 +24,9 @@ def createMarkers(graph):
         num = str(i)
         koordinat = str(coor.x) + " " + str(coor.y)
         folium.Marker([coor.x, coor.y],
-                    popup=heu[i-1],
-                    tooltip="Click for more info",
-                    icon=plugins.BeautifyIcon(number=i,
+                      popup=heu[i-1],
+                      tooltip="Click for more info",
+                      icon=plugins.BeautifyIcon(number=i,
                                                 border_color='blue',
                                                 border_width=2,
                                                 text_color='red',
@@ -34,9 +35,13 @@ def createMarkers(graph):
 
 
 # Create map object
-g1 = convertTextToGraph("../test/ITB.txt")
+g1 = convertTextToGraph("../test/AlunAlun.txt")
 
-map1 = folium.Map(location=[-6.892650, 107.610433], zoom_start=20)
+# peta ITB
+# map1 = folium.Map(location=[-6.892650, 107.610433], zoom_start=20)
+
+# peta AlunAlun
+map1 = folium.Map(location=[-6.9219, 107.6070], zoom_start=20)
 
 
 numOfNode = g1.getNumOfNode()
@@ -54,23 +59,42 @@ while idxFrom < 1 or idxFrom > numOfNode or idxTo < 1 or idxTo > numOfNode:
 
 createMarkers(g1)
 
+#  peta ITB
+# route_Graph = [
+#     [-6.892650, 107.610433],  # 1
+#     [-6.892650, 107.608763],  # 2
+#     [-6.891056, 107.608712],  # 3
+#     [-6.891057, 107.609713],  # 4
+#     [-6.891037, 107.611024],  # 6
+#     [-6.891057, 107.609713],  # 4
+#     [-6.891934, 107.610388],  # 5
+#     [-6.891362, 107.611052],  # 8
+#     [-6.891037, 107.611024],  # 6
+#     [-6.890978, 107.612087],  # 7
+#     [-6.891355, 107.612193],  # 9
+#     [-6.891362, 107.611052],  # 8
+#     [-6.891355, 107.612193],  # 9
+#     [-6.892427, 107.612027],  # 10
+#     [-6.892650, 107.610433],  # 1
+#     [-6.891934, 107.610388]   # 5
+# ]
+
+# peta AlunAlun
 route_Graph = [
-    [-6.892650, 107.610433],  # 1
-    [-6.892650, 107.608763],  # 2
-    [-6.891056, 107.608712],  # 3
-    [-6.891057, 107.609713],  # 4
-    [-6.891037, 107.611024],  # 6
-    [-6.891057, 107.609713],  # 4
-    [-6.891934, 107.610388],  # 5
-    [-6.891362, 107.611052],  # 8
-    [-6.891037, 107.611024],  # 6
-    [-6.890978, 107.612087],  # 7
-    [-6.891355, 107.612193],  # 9
-    [-6.891362, 107.611052],  # 8
-    [-6.891355, 107.612193],  # 9
-    [-6.892427, 107.612027],  # 10
-    [-6.892650, 107.610433],  # 1
-    [-6.891934, 107.610388]   # 5
+    [-6.921233, 107.607760], #1
+    [-6.921036, 107.606462], #2
+    [-6.920924, 107.605099], #3
+    [-6.920855, 107.604080], #4
+    [-6.922075, 107.604022], #5
+    [-6.922405, 107.606410], #8
+    [-6.922075, 107.604022], #5
+    [-6.923119, 107.603942], #6
+    [-6.923417, 107.606222], #7
+    [-6.922405, 107.606410], #8
+    [-6.922565, 107.607628], #9
+    [-6.921233, 107.607760], #1
+    [-6.922565, 107.607628], #9
+    [-6.922687, 107.608433] #10
 ]
 
 pathAndHeu = findPath(g1, idxFrom-1, idxTo-1)
